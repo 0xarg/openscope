9118f2e6eae0de451374273098b9c9de0618ee16
anurag8484 <anuragpoonia.ap@gmail.com>
2025-12-31 19:36:34 +0530

    configured prisma client

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,33 +1,10 @@
-// // import { PrismaClient
-
-// //  } from "@/app/generated/prisma/client";
-
-// import { PrismaClient } from "@prisma/client"
-
-// import { PrismaNeonHttp } from "@prisma/adapter-neon";
-// import { neon } from "@neondatabase/serverless";
-
-//  const prismaClientSingleton = () => {
-//     return new PrismaClient();
-//  }
-
-
-//  type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
-
-//  const globalForPrisma = globalThis as unknown as {
-//     prisma: PrismaClientSingleton | undefined
-//  };
-
-//  const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
-
-//  export default prisma;
-
-//  if (process.env.NODE_ENV!=='production') globalForPrisma.prisma = prisma;
-
-
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
 import { PrismaNeon } from "@prisma/adapter-neon";
 
-const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL });
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({
+  adapter: new PrismaNeon({
+    connectionString: process.env.DATABASE_URL!,
+  }),
+});
+
 export default prisma;
