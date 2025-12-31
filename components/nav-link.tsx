0a28b95a4d9aef84e7346a2a38e7036c1d74de3f
anurag8484 <anuragpoonia.ap@gmail.com>
2026-01-01 00:57:38 +0530

    fixed:logo

--- components/nav-link.tsx
+++ components/nav-link.tsx
@@ -1,28 +1,39 @@
-"use client"
+"use client";
 
-import Link, { type LinkProps } from "next/link"
-import { usePathname } from "next/navigation"
-import { forwardRef, type AnchorHTMLAttributes } from "react"
-import { cn } from "@/lib/utils"
+import Link, { type LinkProps } from "next/link";
+import { usePathname } from "next/navigation";
+import { forwardRef, type AnchorHTMLAttributes } from "react";
+import { cn } from "@/lib/utils";
 
-interface NavLinkProps extends Omit<AnchorHTMLAttributes<HTMLAnchorElement>, keyof LinkProps>, LinkProps {
-  activeClassName?: string
-  pendingClassName?: string
+interface NavLinkProps
+  extends Omit<AnchorHTMLAttributes<HTMLAnchorElement>, keyof LinkProps>,
+    LinkProps {
+  activeClassName?: string;
+  pendingClassName?: string;
 }
 
 const NavLink = forwardRef<HTMLAnchorElement, NavLinkProps>(
-  ({ className, activeClassName, pendingClassName, href, children, ...props }, ref) => {
-    const pathname = usePathname()
-    const isActive = pathname === href
+  (
+    { className, activeClassName, pendingClassName, href, children, ...props },
+    ref
+  ) => {
+    const pathname = usePathname();
+    const isActive = pathname === href;
 
     return (
-      <Link ref={ref} href={href} className={cn(className, isActive && activeClassName)} {...props}>
+      <Link
+        ref={ref}
+        href={href}
+        target="blank"
+        className={cn(className, isActive && activeClassName)}
+        {...props}
+      >
         {children}
       </Link>
-    )
-  },
-)
+    );
+  }
+);
 
-NavLink.displayName = "NavLink"
+NavLink.displayName = "NavLink";
 
-export { NavLink }
+export { NavLink };
